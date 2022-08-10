44eaf2047e0d5661220c98476278ae9be265995e
magnifiques <arpit.vaghela@icloud.com>
2022-08-10 22:30:13 +0530

    add player class

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
 const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const canvasContext = canvas.getContext("2d");
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    (this.width = 100), (this.height = 100);
+  }
+
+  draw() {
+    canvasContext.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+const astro = new Player();
+astro.draw();
 
 console.log(canvasContext);
